c085b24c0f4e2ea5a6b1628c4884dc4aaad9cfef
Evgeny Kasian <ekasian@outlook.com>
2018-05-18 09:17:47 -0700

    Created function to retrieve and set categories from MockData

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,13 @@
 const addIncomeBtn = document.querySelector('.income-btn');
 const formsDiv = document.querySelector('.forms');
 const closeFormBtn = document.querySelector('.close-btn');
+const budgetedMoneyValue = document.querySelector('.money-budgeted-value');
+const formCategoryDropDown = document.querySelector('#category');
 
 
 // STATE
 const BUDGET = {
-  budget: 0,
+  budget: 2400,
   transactions: [{
     category: 'Bills',
     title: 'Shaw Internet',
@@ -14,11 +16,54 @@ const BUDGET = {
     spent: 0
   }],
   categories: {
-    bills: ['Bc Hydro', 'Shaw']
+    bills: ['Bc Hydro', 'Shaw'],
+    restaurants: [],
+    pet: []
   }
 }
 
 // FUNCTIONS
+function retrieveCategories() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const categories = Object.keys(BUDGET.categories);
+      resolve(categories);
+    }, 500)
+  })
+}
+
+async function setCategories() {
+  try {
+    const categories = await retrieveCategories();
+    for (let category of categories) {
+      let newOption = document.createElement('option');
+      newOption.setAttribute('value', category);
+      let categoryText = document.createTextNode(category);
+      newOption.appendChild(categoryText);
+      formCategoryDropDown.appendChild(newOption);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+
+
+function retrieveBudgetValue() {
+  return new Promise((resolve, reject) => {
+    setInterval(() => {
+      resolve(BUDGET.budget + '$');
+    }, 500)
+  })
+}
+
+async function setBudgetValue() {
+  try {
+    budgetedMoneyValue.textContent = await retrieveBudgetValue();
+  } catch (err) {
+    console.error(err)
+  }
+}
 
 
 // EVENT LISTENERS
@@ -28,4 +73,15 @@ addIncomeBtn.on('click', function () {
 })
 closeFormBtn.on('click', function () {
   formsDiv.classList.toggle('hidden');
+})
+
+// DOM RENDERING
+
+function renderState() {
+  setBudgetValue();
+  setCategories();
+}
+
+window.on('load', function (event) {
+  renderState();
 })
