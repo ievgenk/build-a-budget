e334a8558387680d78243f5fa2c461eef3745504
Evgeny Kasian <ekasian@outlook.com>
2018-05-17 19:34:23 -0700

    created a form for submitting transactions

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,7 @@
 //DOM
-const addIncomeBtn = $('.income-btn');
+const addIncomeBtn = document.querySelector('.income-btn');
+const formsDiv = document.querySelector('.forms');
+const closeFormBtn = document.querySelector('.close-btn');
 
 
 // STATE
@@ -11,12 +13,19 @@ const BUDGET = {
     budgeted: 80,
     spent: 0
   }],
-
+  categories: {
+    bills: ['Bc Hydro', 'Shaw']
+  }
 }
+
+// FUNCTIONS
 
 
 // EVENT LISTENERS
 
 addIncomeBtn.on('click', function () {
-  console.log('Clicked')
+  formsDiv.classList.toggle('hidden');
+})
+closeFormBtn.on('click', function () {
+  formsDiv.classList.toggle('hidden');
 })
